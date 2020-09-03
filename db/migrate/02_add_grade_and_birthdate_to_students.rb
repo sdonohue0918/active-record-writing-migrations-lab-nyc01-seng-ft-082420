1db61ac49634810ffcebca84fbec1a30cfbe8192
sdonohue0918 <sdonohue0918@gmail.com>
2020-09-02 20:24:44 -0400

    tests not passing

---
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -0,0 +1,12 @@
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
+    def change
+        add_column :student, :grade, :integer
+        add_column :student, :birthdate, :string
+
+
+
+    
+    end
+
+
+end
